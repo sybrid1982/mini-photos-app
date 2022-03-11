876cc6c85549156c1d110803046187e728cd83d7
Sydney Bridges <sybrid@gmail.com>
2022-03-11 14:39:58 -0500

    Added function to fetch mini by id

--- src/services/minis.service.js
+++ src/services/minis.service.js
@@ -9,3 +9,8 @@ export function createMiniForGame(mini) {
     const url = `/minis`;
     return httpCommon.post(url, mini).then(x => x.data);
 }
+
+export function fetchMiniById(id) {
+    const url = `/minis/${id}`;
+    return httpCommon.get(url).catch(error => console.error(error)).then(x => x.data);
+}
