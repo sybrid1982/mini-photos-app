6582cff9f5d5e16699eda01e837c8d63126b5774
Sydney Bridges <sybrid@gmail.com>
2022-03-11 14:31:02 -0500

    put catch for error

--- src/services/games.service.js
+++ src/services/games.service.js
@@ -2,12 +2,12 @@ import httpCommon from "../http-common";
 
 export function fetchGames() {
     const url = `/games`;
-    return httpCommon.get(url).then(x => x.data)
+    return httpCommon.get(url).catch((error => error.response.status)).then(x => x.data)
 }
 
 export function fetchGame(id) {
     const url = `/games/${id}`;
-    return httpCommon.get(url).then(x => x.data)
+    return httpCommon.get(url).catch((error => error.response.status)).then(x => x.data)
 }
 
 export function postGame(gameData, filename) {
