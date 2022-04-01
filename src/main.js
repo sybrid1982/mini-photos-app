9a81d719df4167f7f8aa78454f236a7fd35cc17a
Sydney Bridges <sybrid@gmail.com>
2022-04-01 13:29:17 -0400

    Removed vue-material import from main.js

--- src/main.js
+++ src/main.js
@@ -5,7 +5,6 @@ import Minis from './components/Minis.vue'
 import Game from './components/Game.vue'
 import Mini from './components/Mini.vue'
 import App from './App.vue'
-import 'vue-material/dist/vue-material.css'
 import vuetify from './plugins/vuetify'
 
 Vue.config.productionTip = false
