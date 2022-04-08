8aadd8a34bbca6b1d97688a42bf6d609162c44a7
Sydney Bridges <sybrid@gmail.com>
2022-04-08 12:14:01 -0400

    Setting up app to deploy in nginx container, connect to api via reverse proxy, and build from circleci

--- Dockerfile
+++ Dockerfile
@@ -6,4 +6,5 @@ COPY . .
 RUN npm run build
 
 FROM nginx as runtime
+COPY nginx.conf /etc/nginx/conf.d/default.conf
 COPY --from=build /app/dist /usr/share/nginx/html
--- src/utility-scripts/transformurl.js
+++ src/utility-scripts/transformurl.js
@@ -1,9 +1,12 @@
 export function transformUrl(url)
 {
-    if(url && url.length > 0 || url === 'string') {
-        return url
+    if(url && typeof(url) === 'string' && url.length > 0) {
+        const localDrive = "localFile";
+        console.log("url was " + url);
+        if(url.startsWith(localDrive)) return url.substring(localDrive.length);
+        return url;
     }
     else {
-        return 'no-image.png'
+        return 'no-image.png';
     }
 }
