6e4ef589419d6c5521ec19b549757bef4182340d
Sydney Bridges <sybrid@gmail.com>
2022-04-14 18:16:43 -0400

    making all the strings single quote in this file for consistency

--- src/utility-scripts/transformurl.js
+++ src/utility-scripts/transformurl.js
@@ -3,7 +3,7 @@ export function transformUrl(url)
     if(url && typeof(url) === 'string' && url.length > 0) {
         const miniPhotos = 'mini-photos'
         if(url.indexOf(miniPhotos) > 0) {
-            return url.replace(miniPhotos, "thumbs");
+            return url.replace(miniPhotos, 'thumbs');
         }
         return url;
     }
